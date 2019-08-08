45a4e8bbc649b17a8849030ce79a63790550da79
Marius Göcke <marius.goecke@gmail.com>
2019-08-08 20:38:08 +0200

    fixes

--- CreateNew.py
+++ CreateNew.py
@@ -1,25 +1,25 @@
 import argparse
 import os
 import sys
-import shutil
+import subprocess
 parser = argparse.ArgumentParser(description='Creates a new formal letter.')
 
-parser.add_argument('--folder_for_new_document', help='Specifies the folder where the new document should be stored')
-parser.add_argument('--document_name', help='Specifies the name of the new document')
+parser.add_argument('folder_for_new_document', help='Specifies the folder where the new document should be stored')
+parser.add_argument('document_name', help='Specifies the name of the new document')
 
 args = parser.parse_args()
+template_folder=os.path.dirname(os.path.abspath(__file__))
 
 def execute(command:str, argument:str):
     print(subprocess.getoutput(command+" "+argument))
 
-new_document_folder=os.path.combine(args.folder_for_new_document,args.document_name)
+new_document_folder=os.path.join(args.folder_for_new_document,args.document_name)
 if(os.path.isdir(new_document_folder)):
-    println(new_document_folder + " does already exist")
+    print(new_document_folder + " does already exist")
     sys.exit(1)
-os.makedir(new_document_folder)
+os.makedirs(new_document_folder)
 os.chdir(new_document_folder)
 execute("git", "init")
-template_folder=os.path.dirname(os.path.abspath(__file__))
 execute("git", "submodule add "+template_folder+" template")
 content_file="documentcontent.tex"
 content_file_content="TODO"
@@ -28,7 +28,7 @@ with open(content_file,'w') as f:
 with open("License.txt",'w') as f:
     f.write("Only the author of the content of '"+content_file+"' is allowed to use the content of this repository.")
 os.chdir("template")
-execute("Build.py")
+execute("Python", "BuildDocument.py")
 os.chdir("..")
 execute("git", 'commit -m "Initial commit"')
 
