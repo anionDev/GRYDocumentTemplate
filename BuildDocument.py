30f6ed4f4166b9841a9e9aa9c0587d968c6d0c51
Marius Göcke <marius.goecke@gmail.com>
2019-08-08 22:34:13 +0200

    updates;added some stuff for readme

--- BuildDocument.py
+++ BuildDocument.py
@@ -1,3 +1,6 @@
 import subprocess
-pdfLatexArgument="\"\input{document.tex}\" -synctex=1 -interaction=nonstopmode -job-name letter -halt-on-error -output-directory .."
+from pathlib import Path
+pdfLatexArgument="\"\input{Template.tex}\" -synctex=1 -interaction=nonstopmode -job-name letter -halt-on-error -output-directory .."
+project_folder_name=os.path.basename(os.path.dirname(os.path.abspath(__file__)))
+os.rename("../Document.tex","../"+project_folder_name+".pdf")
 subprocess.call("pdflatex " + pdfLatexArgument)
--- ReadMe.md
+++ ReadMe.md
@@ -1,1 +1,14 @@
+# What is this
+
 This repository contains a template for a formal letter. It is designed to be used as git-submodule.
+
+# Getting started
+
+TODO
+
+# Requirements
+
+To get this scripts working the following command must be available on your system:
+* python
+* git
+* pdflatex
