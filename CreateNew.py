14226d8685ef9c3f643646b99a5ea04b7706e2e4
Marius Göcke <marius.goecke@gmail.com>
2019-08-12 11:13:52 +0200

    updates

--- CreateNew.py
+++ CreateNew.py
@@ -11,9 +11,10 @@ parser.add_argument('folder_for_new_document', help='Specifies the folder where 
 parser.add_argument('document_name', help='Specifies the name of the thesis')
 
 args = parser.parse_args()
+template_repository=os.path.dirname(os.path.abspath(__file__))
 
-template_repository=os.path.dirname(os.path.abspath(__file__))# use this path to use this (local) repository as remote for the submodule
-#template_repository="https://github.com/anionDev/gryThesis.git"
+template_remote_repository=template_repository# use this path to use this (local) repository as remote for the submodule
+#template_remote_repository="https://github.com/anionDev/gryThesis.git"
 
 def execute(command:str, argument:str):
     print(subprocess.getoutput(command+" "+argument))
@@ -25,8 +26,8 @@ if(os.path.isdir(new_document_folder)):
 os.makedirs(new_document_folder)
 os.chdir(new_document_folder)
 execute("git", "init")
-execute("git", "submodule add "+template_repository+" template")
-content_file_content="\input{../Content/thesis-content}"
+execute("git", "submodule add "+template_remote_repository+" template")
+content_file_content="\\input{../Content/thesis-content}"
 with open("License.txt",'w') as f:
     f.write("Only the author of the content of this thesis is allowed to use the content of this repository.")
 with open("metadata.tex",'w') as f:
@@ -34,7 +35,7 @@ with open("metadata.tex",'w') as f:
     "\\newcommand{\\thesisname}{"+args.document_name+"}\n"+
     "\\newcommand{\\customfooter}{Please report any mistakes here: \\url{https://example.com/mythesis}}")
     urllib.request.urlretrieve('https://raw.githubusercontent.com/github/gitignore/master/TeX.gitignore', os.path.join(new_document_folder,".gitignore"))
-shutil.copytree("Template", os.path.join(new_document_folder,"Content"))
+shutil.copytree(os.path.join(template_repository,"Template"), os.path.join(new_document_folder,"Content"))
 os.chdir("template")
 execute("Python", "BuildDocument.py")
 os.chdir("..")
