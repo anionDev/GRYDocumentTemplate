c985f53566088cbf4d48d542100fbdc428615972
Marius Göcke <marius.goecke@gmail.com>
2020-04-23 20:31:33 +0200

    added latexmk;removed buildscript

--- BuildDocument.py
+++
@@ -1,8 +0,0 @@
-from argparse import ArgumentParser
-import os
-import subprocess
-parser = ArgumentParser()
-parser.add_argument("-pdf", dest="pdflatexfile", help="pdfLatex file with full path", default="pdflatex")
-args = parser.parse_args()
-pdfLatexArgument="\"\input{Thesis.tex}\" -synctex=1 -interaction=nonstopmode -job-name Thesis -halt-on-error -output-directory .."
-subprocess.call(args.pdflatexfile + " " + pdfLatexArgument)
