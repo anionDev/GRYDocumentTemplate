cbad11c3214afe12ff38aa44e2cc3fe84b7a9c3c
Marius Göcke <marius.goecke@gmail.com>
2020-08-18 18:03:59 +0200

    some updates for create-script

--- Scripts/CreateNewRepository/CreateNewRepository.py
+++ Scripts/CreateNewRepository/CreateNewRepository.py
@@ -1,13 +1,30 @@
 import argparse
+import sys
+import os
+from ScriptCollection.core import write_message_to_stderr, execute_and_raise_exception_if_exit_code_is_not_zero, write_exception_to_stderr
 
 parser = argparse.ArgumentParser(description="Creates a new repository with an example-thesis")
-parser.add_argument(required=True)
-parser.add_argument('-a', '--author', required=False)
-parser.add_argument('-t', '--title', required=False)
-parser.add_argument('-d', '--documenttype', required=False)
+parser.add_argument("folder")
+parser.add_argument('-a', '--author')
+parser.add_argument('-t', '--title')
+parser.add_argument('-d', '--date')
+parser.add_argument('-y', '--documenttype')
 args = parser.parse_args()
 
-def execute_program(folder:str,program:str,arguments:str)
-    pass
+def process(arguments):
+    try:
+        if(os.path.isdir(arguments.folder)):
+            write_message_to_stderr(f"The directory '{arguments.folder}' already exists")
+            return 1
+        else:
+            os.makedirs(arguments.folder)
+        execute_and_raise_exception_if_exit_code_is_not_zero("git","init", arguments.folder)
+        
+        # TODO
+        
+    except Exception as exception:
+        write_exception_to_stderr(exception)
+        return 1
+    return 0
 
-
+sys.exit(process(args))
