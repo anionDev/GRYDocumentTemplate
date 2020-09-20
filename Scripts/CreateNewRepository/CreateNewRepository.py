04e1886f47a1d77e191bac6020ce77078b1886be
Marius Göcke <marius.goecke@gmail.com>
2020-09-20 21:46:06 +0200

    some updates in createnewrepositoryscript

--- Scripts/CreateNewRepository/CreateNewRepository.py
+++ Scripts/CreateNewRepository/CreateNewRepository.py
@@ -1,26 +1,31 @@
 import argparse
 import sys
 import os
-from ScriptCollection.core import write_message_to_stderr, execute_and_raise_exception_if_exit_code_is_not_zero, write_exception_to_stderr
+from distutils.dir_util import copy_tree
+from ScriptCollection.core import write_message_to_stderr, execute_and_raise_exception_if_exit_code_is_not_zero, write_exception_to_stderr, ensure_directory_exists, resolve_relative_path, git_commit, resolve_relative_path_from_current_working_directory
 
-parser = argparse.ArgumentParser(description="Creates a new repository with an example-thesis")
-parser.add_argument("folder")
-parser.add_argument('-a', '--author')
-parser.add_argument('-t', '--title')
-parser.add_argument('-d', '--date')
-parser.add_argument('-y', '--documenttype')
-args = parser.parse_args()
 
 def process(arguments):
     try:
-        if(os.path.isdir(arguments.folder)):
-            write_message_to_stderr(f"The directory '{arguments.folder}' already exists")
-            return 1
+        parser = argparse.ArgumentParser(description="Creates a new repository for a thesis")
+        parser.add_argument("folder")
+        parser.add_argument('-a', '--author')
+        parser.add_argument('-t', '--title')
+        parser.add_argument('-d', '--date')
+        parser.add_argument('-y', '--documenttype')
+        args = parser.parse_args()
+        folder=resolve_relative_path_from_current_working_directory(arguments.folder)
+        folder_of_current_file = os.path.dirname(os.path.realpath(__file__))
+        folder_of_current_repository=resolve_relative_path(f"..{os.path.sep}..",folder_of_current_file)
+        if(os.path.isdir(folder)):
+            write_message_to_stderr(f"The directory '{folder}' already exists")
+            return 2
         else:
-            os.makedirs(arguments.folder)
-        execute_and_raise_exception_if_exit_code_is_not_zero("git","init", arguments.folder)
-        
-        # TODO
+            ensure_directory_exists(folder)
+            execute_and_raise_exception_if_exit_code_is_not_zero("git","init", folder)
+            copy_tree(os.path.join(folder_of_current_file, "Template"), os.path.join(folder_of_current_file, "Thesis", "Template"))
+            execute_and_raise_exception_if_exit_code_is_not_zero("git",f'submodule add -b development "{folder_of_current_repository}" Thesis{os.path.sep}Template', folder)
+            git_commit(folder, "Initial commit")
         
     except Exception as exception:
         write_exception_to_stderr(exception)
