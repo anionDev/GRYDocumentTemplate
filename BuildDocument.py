1b102e12e76589337c1578df7101823b6b0d375a
Marius Göcke <marius.goecke@gmail.com>
2020-09-21 20:50:27 +0200

    removed obsolete content

--- BuildDocument.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-import time
-from pathlib import Path
-pdfLatexArgument="\"\input{Template.tex}\" -synctex=1 -interaction=nonstopmode -job-name letter -halt-on-error -output-directory .."
-subprocess.call("pdflatex " + pdfLatexArgument)
-project_folder_name=os.path.basename(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-os.chdir("..")
-os.rename("letter.pdf",project_folder_name+".pdf")
