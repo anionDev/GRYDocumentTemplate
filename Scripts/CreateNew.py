34baa273bb762d18e1580a9b30af7b0a81e40324
Marius Göcke <marius.goecke@gmail.com>
2020-09-21 23:19:04 +0200

    improvement in createnew-script

--- Scripts/CreateNew.py
+++ Scripts/CreateNew.py
@@ -35,35 +35,40 @@ def process(arguments):
         folder_of_current_repository=resolve_relative_path(f"..",folder_of_current_file)
         folder_of_common_files=resolve_relative_path(f"CommonFiles",folder_of_current_repository)
         folder_of_concrete_template=resolve_relative_path(f"Templates{os.path.sep}{arguments.template}",folder_of_current_repository)
+
+        if(not os.path.isdir(folder_of_concrete_template)):
+            write_message_to_stderr(f"'{arguments.template}' is not a known template.")
+            return 3
+            
         if(os.path.isdir(folder)):
-            write_message_to_stderr(f"The directory '{folder}' already exists")
+            write_message_to_stderr(f"The directory '{folder}' already exists.")
             return 2
-        else:
-            ensure_directory_exists(folder)
             
-            execute_and_raise_exception_if_exit_code_is_not_zero("git","init", folder)
-            copy_tree(folder_of_common_files, folder)
-            document_folder=os.path.join(folder, "Document")
-            
-            execute_and_raise_exception_if_exit_code_is_not_zero("git",f'submodule add -b development "{folder_of_current_repository}" Document{os.path.sep}GRYDocumentTemplate', folder)
-            copy_tree(os.path.join(folder_of_concrete_template, "TemplateFiles", "Copy"), document_folder)            
-            
-            replacements=dict()
-            replacements["author"]=arguments.author
-            replacements["name"]=arguments.name
-            replacements["documenttype"]=arguments.template
-            replacements["day"]='{:02d}'.format(datetime.date.today().day)
-            replacements["month"]='{:02d}'.format(datetime.date.today().month)
-            replacements["year"]=str(datetime.date.today().year)
-            
-            replace_underscores_in_file(os.path.join(folder, "License.txt"),replacements)
-            replace_underscores_in_file(os.path.join(folder, "ReadMe.md"),replacements)
-            for line in read_lines_from_file(os.path.join(folder_of_concrete_template,"FilesWithReplacements.txt")):
-                if not string_is_none_or_whitespace(line):
-                    replace_underscores_in_file(os.path.join(document_folder, line),replacements)
-            
-            execute_and_raise_exception_if_exit_code_is_not_zero("arara","document.tex", document_folder)            
-            git_commit(folder, "Initial commit")
+        ensure_directory_exists(folder)
+        
+        execute_and_raise_exception_if_exit_code_is_not_zero("git","init", folder)
+        copy_tree(folder_of_common_files, folder)
+        document_folder=os.path.join(folder, "Document")
+        
+        execute_and_raise_exception_if_exit_code_is_not_zero("git",f'submodule add -b development "{folder_of_current_repository}" Document{os.path.sep}GRYDocumentTemplate', folder)
+        copy_tree(os.path.join(folder_of_concrete_template, "TemplateFiles", "Copy"), document_folder)            
+        
+        replacements=dict()
+        replacements["author"]=arguments.author
+        replacements["name"]=arguments.name
+        replacements["documenttype"]=arguments.template
+        replacements["day"]='{:02d}'.format(datetime.date.today().day)
+        replacements["month"]='{:02d}'.format(datetime.date.today().month)
+        replacements["year"]=str(datetime.date.today().year)
+        
+        replace_underscores_in_file(os.path.join(folder, "License.txt"),replacements)
+        replace_underscores_in_file(os.path.join(folder, "ReadMe.md"),replacements)
+        for line in read_lines_from_file(os.path.join(folder_of_concrete_template,"FilesWithReplacements.txt")):
+            if not string_is_none_or_whitespace(line):
+                replace_underscores_in_file(os.path.join(document_folder, line),replacements)
+        
+        execute_and_raise_exception_if_exit_code_is_not_zero("arara","document.tex", document_folder)            
+        git_commit(folder, "Initial commit")
         
     except Exception as exception:
         write_exception_to_stderr(exception)
